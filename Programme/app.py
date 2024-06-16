fea3d4ad37f7cb33614ab8f7fb038adc6e1d3154
Ishem <123887246+IshemZ@users.noreply.github.com>
2024-06-16 22:07:19 +0200

    selection des extensions

--- Programme/app.py
+++ Programme/app.py
@@ -1,7 +1,10 @@
+import shutil
 import os
+import filetype
 
-#import de la variable qui contient l'absolute path
-from path import source_dir 
+#import de la variable qui contient mon directory à déplacer
+from path import source_dir, destination_dir
+
 
 
 #Etape 1 : Détérminer les catégories ainsi que les extensions associés
@@ -14,15 +17,19 @@ File_type = {
     "Excel" : [".xlsx", ".csv", ".xls"],
     "Powerpoint" : [".pptx"],
     "Scripts" : [".sh"],
-    "Divers" : [".drawio", ".rdp", ".DS_Store"]
+    "Divers" : [".drawio", ".rdp", ".DS_Store"],
+    "SansExtension" : []
+
 }
 
+#Création des catégories
+for category in File_type.keys():
+    os.makedirs(os.path.join(destination_dir, category), exist_ok=True)
+
 # os.scandir() est plus efficace que os.Listdir()
-
-#Je liste tout les fichiers de mon répértoire de départ
-
 all_unique_entry = os.scandir(source_dir)
 
+#Etape 2 : Je liste tout les fichiers de mon répértoire de départ
 with all_unique_entry as entries:
     for entry in entries:
         if entry.is_file():
@@ -32,3 +39,32 @@ with all_unique_entry as entries:
         else:
             raise TypeError(f"Liste des éléement qui ne son ni un fichier ni un dossier : {entry.name} avec comme chemin : {entry.path}")
 
+all_unique_entry = os.scandir(source_dir)
+
+with all_unique_entry as entries:
+    for entry in entries:
+        filename, ext = os.path.splitext(entry.name)
+        if not ext:
+            if entry.is_dir():
+                pass
+            elif entry.is_file():
+                try:
+                    # Utilisation de filetype pour déterminer le type de fichier
+                    kind = filetype.guess(entry.path)
+                    if kind is None:
+                        file_type = 'Type inconnu'
+                    else:
+                        file_type = kind.mime
+                    print(f"Fichier sans extension: {filename}, Type MIME: {file_type}")
+                except Exception as e:
+                    print(f"Erreur lors de l'accès au fichier {entry.name}: {e}")
+        elif ext:
+            if entry.is_file():
+                print(f"Fichier avec extension: {filename} possède une extension explicite ({ext})")
+            elif entry.is_dir():
+                pass
+
+#Etape 3 : import de la variable qui contient mon directory à déplacer
+
+for category in File_type.keys():
+    os.makedirs(os.path.join(destination_dir, category), exist_ok=True)
